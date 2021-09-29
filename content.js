48eda3c12bf98e46f172dec7fcd6b3ee6d8b569b
Arlind Rexhmataj <arexhmataj@ritech.co>
2021-09-29 15:43:30 +0200

    Removed pirate bay icon AGAIN

--- content.js
+++ content.js
@@ -11,7 +11,7 @@ if(Object.values(imdbList).includes(locList)){
         let str = item[i].getElementsByTagName('a')[0].href;
         let res = str.split("title/"); 
         let id=res[1].split("/")[0];
-        item[i].innerHTML +='<span class="lister-item-year text-muted" style="margin-left:10px;margin-right:10px;">'+ '<a href=https://tpblautbay.one/search.php?q=' + id +' target="_blank">'+'<img src='+imgURL2+' alt="TPB">'+'</a>'+'</span>'+'<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + id +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
+        item[i].innerHTML +='<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + id +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
     }
 } else {
     locId=loc[1].split('/')[1];
@@ -22,7 +22,7 @@ if(Object.values(imdbList).includes(locList)){
         let item = document.getElementsByClassName("title_wrapper")[0];
         let h1 = document.getElementsByClassName("TitleHeader__TitleText-sc-1wu6n3d-0")[0];
         let node = document.createElement("h1");
-        node.innerHTML='<span class="lister-item-year text-muted" style="margin-right:10px;">'+ '<a href=https://tpblautbay.one/search.php?q=' + locId +' target="_blank">'+'<img src='+imgURL2+' alt="TPB">'+'</a>'+'</span>'+'<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + locId +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
+        node.innerHTML='<span class="lister-item-year text-muted">'+ '<a href=https://rarbgaccess.org/torrents.php?search=' + locId +' target="_blank">'+'<img src='+imgURL1+' alt="RARBG">'+'</a>'+'</span>';
         h1.innerHTML+=node.innerHTML;
     }
 }
